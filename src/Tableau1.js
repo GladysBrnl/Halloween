db9a3f20e7644ed615c922cd729ea9b21aa3e33b
GladysBrnl <92451562+GladysBrnl@users.noreply.github.com>
2021-10-31 10:53:29 +0100

    Reprise Halloween
    Le fichier précédent ne pouvant afficher le contenu, on reprend depuis le début pour  vérifier

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -25,12 +25,13 @@ class Tableau1 extends Phaser.Scene{
             this.load.image('g-grass-'+i, 'assets/level/ground/g-grass-'+i+'.png');
         }
 
-        for(let i=1; i<=3;i++) {
-            this.load.image('filterFilm' +i, 'assets/level/filters/film/frame-'+i+'.png');
-        }
-        for(let i=1;i<=3;i++) {
-            this.load.image('bg-animation-'+i, 'assets/level/background-2/bg-animation/bg-animation-'+i+'png');
-        }
+       //filtre film TODO élève : faire une boucle à la place des 3 lignes qui suivent
+        this.load.image('filterFilm1', 'assets/level/filters/film/frame-1.png');
+        this.load.image('filterFilm2', 'assets/level/filters/film/frame-2.png');
+        this.load.image('filterFilm3', 'assets/level/filters/film/frame-3.png');
+
+        //texture au fond  TODO élève : faire une boucle pour charger les 3 images et démontrer par la même que vous savez aller au plus simple
+        this.load.image('bg-animation-a', 'assets/level/background-2/bg-animation/bg-animation-a.png');
 
     }
 
