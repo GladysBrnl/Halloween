c20e781f68fb8f3148f12bed70abdd1b7d42d2d2
GladysBrnl <brunelgladys@gmail.com>
2021-11-02 15:38:04 +0100

    Halloween avancée v2

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -11,6 +11,7 @@ class Tableau1 extends Phaser.Scene{
         this.load.image('bg2-terrain-2', 'assets/level/background-2/bg2-terrain-2.png');
         this.load.image('bg2-terrain-1', 'assets/level/background-2/bg2-terrain-1.png')
         this.load.image('bg2-tree-2', 'assets/level/background-2/bg2-tree-2.png');
+        this.load.image('bg2-tree-3', 'assets/level/background-2/bg2-tree-3.png');
 
         //bg 1 (gris légèrement flou)
         this.load.image('bg1-terrain-3', 'assets/level/background-1/bg-terrain-3.png');
@@ -59,14 +60,14 @@ class Tableau1 extends Phaser.Scene{
         this.bg2Container=this.add.container(0,0);
 
         /**
-         * Terrain dans bg2
+         * Terrain2 dans bg2
          * @type {Phaser.GameObjects.Image}
          */
         let bg2Terrain2=this.add.image(-100,100, 'bg2-terrain-2').setOrigin(0,0);
         this.bg2Container.add(bg2Terrain2);
 
         /**
-         * Terrain dans bg2
+         * Terrain1 dans bg2
          *  @type {Phaser.GameObjects.Container}
          */
         let bg2Terrain1=this.add.image(-300,100,'bg2-terrain-1').setOrigin(0,0);
@@ -78,6 +79,16 @@ class Tableau1 extends Phaser.Scene{
         let bg2Tree2=this.add.image(400,-50, 'bg2-tree-2').setOrigin(0,0);
         this.bg2Container.add(bg2Tree2);
         bg2Tree2.angle=-5; //pencher l'arbre de -5 degrès
+
+        /**
+         * Arbre dans bg2
+         * @type {Phaser.GameObjects.Image}
+         */
+        let bg2Tree3=this.add.image(600,-100,'bg2-tree-3').setOrigin(0,0);
+        this.bg2Container.add(bg2Tree3);
+        bg2Tree3.angle=-15;
+
+
 
         //--------------background 1 (gris) --------------------
 
